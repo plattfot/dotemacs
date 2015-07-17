783542314a2daf193d9bad4be51c44b9100ebc7f
plattfot <fredriks@d2.com>
2015-07-17 11:40:11 -0700

    Added pid and attach function for smeat

--- init.d/debug-functions.el
+++ init.d/debug-functions.el
@@ -2,7 +2,20 @@
 ;; Debug Functions:
 ;; Lisp functions for debugging
 ;; =============================================================================
-
+;; ---------------------------- PID of process ---------------------------------
+(defun pid (regex &optional index)
+"Get the pid of REGEX, if more than one is running it returns one
+at INDEX. Where INDEX starts from 0 and up"
+(interactive)
+(when (not index) (setq index 0))
+(nth index 
+ (split-string 
+  (shell-command-to-string
+   (concat "ps aux | " ;; wrap first character in [ ] to not match itself
+	   "sed -nE \"s/$USER\\s+([0-9]+).*?"
+	   (concat "[" (substring regex 0 1) "]" (substring regex 1))"/\\1/p\"")
+   )))
+)
 ;; ----------------------------- PID Houdini -----------------------------------
 (defun pid-houdini (&optional index )
 "Get the pid for houdini, if more than one is running it returns
@@ -10,12 +23,7 @@ the one at INDEX. Where INDEX starts from 0 and up"
 
 (interactive)
 (when (not index) (setq index 0))
-(nth index 
- (split-string 
-  (shell-command-to-string
-   (concat "ps aux | " ;; [h]oudini to not match itself
-	   "sed -nE \"s/$USER\\s+([0-9]+).*?[h]oudini-bin/\\1/p\"")
-   )))
+(pid "houdini-bin" index)
 )
 ;; ---------------------------- Attach Houdini ---------------------------------
 (defun attach-houdini (&optional index) 
@@ -42,12 +50,7 @@ first on the ps list."
 the one at INDEX. Where INDEX starts from 0 and up"
 (interactive)
 (when (not index) (setq index 0))
-(nth index
- (split-string 
-  (shell-command-to-string
-   (concat "ps aux | " ;; [h]oudini to not match itself
-	   "sed -nE \"s/$USER\\s+([0-9]+).*?[m]aya\.bin/\\1/p\"")
-   )))
+(pid "maya\\.bin" index)
 )
 
 ;; ----------------------------- Attach Maya -----------------------------------
@@ -66,6 +69,27 @@ which one if there are multiple instances running, INDEX counts from 1."
 first on the ps list."
 (interactive)
 (shell-command (concat "kill -9 " (pid-maya)))
+)
+
+;; ------------------------------ PID SMEAT ------------------------------------
+(defun pid-smeat (&optional index)
+"Get the pid for smeat, if more than one is running it returns
+the one at INDEX. Where INDEX starts from 0 and up"
+(interactive)
+(when (not index) (setq index 0))
+;; the ^= is to ignore houdini/python commands e.g houdini --with smeat=...
+(pid "smeat(:?[^=]+|$$)" index)
+)
+
+;; ---------------------------- Attach Smeat -----------------------------------
+(defun attach-smeat (&optional index) 
+"prints attach <pid> into the buffer. INDEX is use to select
+which one if there are multiple instances running, INDEX counts from 1."
+(interactive"p")
+
+;; The default for index is one.
+(when (< index 1) (setq index 1))
+(insert-string (concat "attach " (pid-smeat (- index 1)) ))
 )
 
 ;; ------------------------------ Preprocess -----------------------------------
