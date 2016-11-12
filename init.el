6399d0cd1d002ad905f3d5a5fb85894605309921
Fredrik Salomonsson <plattfot@gmail.com>
2016-11-12 09:26:15 -0800

    Auto variables from package

--- init.el
+++ init.el
@@ -19,3 +19,17 @@
       (dotemacs/load-user-file "work/work.el")
       (dotemacs/load-user-file "work/dd-newfile.el")
  ) nil )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (esup yasnippet yaml-mode sudo-edit string-inflection rtags org-bullets multi-term move-text mc-extras magit ledger-mode go-autocomplete gnuplot flycheck expand-region evil-numbers buffer-move ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
