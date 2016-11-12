f01f8a77846664f3c6e399bb4839a9bd14e05c82
Fredrik Salomonsson <plattfot@gmail.com>
2016-11-11 16:43:17 -0800

    Finished with some more files

--- init.d/go.el
+++
@@ -1,9 +0,0 @@
-(add-hook 'go-mode-hook 
-  (lambda ()
-    (setq-default) 
-    (setq tab-width 2) 
-    (setq standard-indent 2) 
-    (setq indent-tabs-mode nil)))
-
-(require 'go-autocomplete)
-(require 'auto-complete-config)
