9be1df045f895e88e290622ab4d934591371d6d9
Fredrik Salomonsson <plattfot@gmail.com>
2016-11-16 22:46:49 -0800

    Added line for bypassing org, but don't notice any noticable difference

--- init.el
+++ init.el
@@ -5,7 +5,22 @@
 (package-initialize)
 
 (org-babel-load-file "~/.emacs.d/init.d/configuration.org")
+;;(load "~/.emacs.d/init.d/configuration")
 
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(package-selected-packages
+;;    (quote
+;;     (use-package esup yasnippet yaml-mode sudo-edit string-inflection rtags org-bullets multi-term move-text mc-extras magit ledger-mode go-autocomplete gnuplot flycheck expand-region evil-numbers buffer-move ag))))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -13,7 +28,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (use-package esup yasnippet yaml-mode sudo-edit string-inflection rtags org-bullets multi-term move-text mc-extras magit ledger-mode go-autocomplete gnuplot flycheck expand-region evil-numbers buffer-move ag))))
+    (yasnippet yaml-mode use-package sudo-edit string-inflection rtags org-bullets multi-term move-text mc-extras magit ledger-mode go-autocomplete gnuplot flycheck expand-region evil-numbers esup buffer-move ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
