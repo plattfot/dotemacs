00560efbfdbc4cab007e3255990ae09463cfff2e
Fredrik Salomonsson <fredriks@d2.com>
2019-01-29 16:52:42 -0800

    Added functions to insert paths to sourceme.py files [work]
    Useful for when loading custom functions, commands etc in gdb.

--- plugins/work.el
+++ plugins/work.el
@@ -187,5 +187,35 @@ of CONFIG as the version."
 
 (set-register ?d (cons 'file work-swdevl))
 
+;; ============================== GDB ========================================
+(define-prefix-command 'gdb-insert-map)
+
+(defun work-insert-sourceme (name &optional config use-config-verbatim)
+  "Insert path to NAME's sourceme file.
+
+CONFIG is where it should look for the version.  It's
+relative to `work-swdevl'.  By default it is CoreLibs.
+
+If USE-CONFIG-VERBATIM is not nil it will just use the value
+of CONFIG as the version.
+
+Useful when debugging."
+  (insert (work-pkg-path name "gdb/sourceme.py" config use-config-verbatim)))
+
+(defun work-insert-spork-sourceme ()
+  "Insert path to spork's sourceme file."
+  (interactive)
+  (work-insert-sourceme "spork_for_gdb" "0.7.0" t))
+
+(defun work-insert-openmesh-sourceme ()
+  "Insert path to spork's sourceme file."
+  (interactive)
+  (work-insert-sourceme "openmesh_for_gdb" "1.0.0" t))
+
+(global-set-key (kbd "C-c i") 'gdb-insert-map)
+
+(define-key gdb-insert-map (kbd "s") 'work-insert-spork-sourceme)
+(define-key gdb-insert-map (kbd "m") 'work-insert-openmesh-sourceme)
+
 (provide 'work)
 ;;; work.el ends here
