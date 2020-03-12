1f97005c370335f84cf2bc1c0bf6cd32e4c5d266
Fredrik Salomonsson <plattfot@gmail.com>
2020-03-12 09:41:06 -0700

    Removing trailing whitespaces [houdini]

--- plugins/houdini.el
+++ plugins/houdini.el
@@ -5,14 +5,14 @@
 (require 'cpreproc)
 
 (defun hou-version-to-hex (version-string)
-  "Convert VERSION-STRING to hex.  
+  "Convert VERSION-STRING to hex.
 Instead of looking up in a table better just to compute it."
   ;; Split the string into parts, filtering out the pre
   (let ((vers_parts (delete "" (split-string version-string "[.]\\|pre[0-9]+"))))
     ;; If it's not three parts, i.e major, minor and build version throw error
     (when (not (eq (length vers_parts) 3))
       (throw 'hou-tag (format "%s is not a valid version string" version-string)))
-    
+
     ;; Convert the parts to integers and then print them out in the
     ;; format houdini is using.
     (let ((vers_int (mapcar #'(lambda (x) (string-to-number x)) vers_parts)))
@@ -26,7 +26,7 @@ ROOT/*/toolkit/include/{UT,SYS}/{UT,SYS}_Version.h for
 UT_VERSION_INT or SYS_VERSION_INT and fetch the INT ."
   (when (not root) (setq root "/tools/package/houdini"))
   (mapcar #'(lambda (x) (split-string x))
-	  (split-string-and-unquote 
+	  (split-string-and-unquote
 	   (shell-command-to-string
 	    (concat "grep -E \"SYS_VERSION_FULL_INT|UT_VERSION_INT\" "
 		    root
@@ -54,7 +54,7 @@ UT_VERSION_INT or SYS_VERSION_INT and fetch the INT ."
   (let ((version_hex (hou-version-to-hex version))) (insert version_hex)))
 
 (defun hou-insert-if (version comp)
-  "Insert an #if-else clause. 
+  "Insert an #if-else clause.
 The specified VERSION in int format with the COMP being either
 ==, <=, >=, !=, < or >.  If a region is active it will place that
 in both the ifdef and else clause.  Example:
@@ -141,7 +141,7 @@ versions it will return the oldest version."
       )))
 
 (defun hou-insert-version (&optional version-offset)
-  "Insert the houdini version where the cursor is located.  
+  "Insert the houdini version where the cursor is located.
 If VERSION-OFFSET is 1 (default) it will insert the latest
 installed houdini version, 2 the next latest and so on.  If the
 offset is less than 1 or greater than the number of installed
@@ -150,12 +150,12 @@ versions it will return the oldest version."
   (insert (hou-get-version version-offset)))
 
 (defun hou-open-other-version (version)
-  "Will open the same hdk file but for the version specified by VERSION.  
+  "Will open the same hdk file but for the version specified by VERSION.
 Note only works if the open buffer is an hdk file."
 (interactive "sEnter houdini version: ")
 (let ((file-path (buffer-file-name))
       (hou-regexp "\\(.*?/houdini/\\)[0-9]+\\.[0-9]+\\.[0-9]+\\(/.*\\)+"))
-  (if file-path 
+  (if file-path
       (progn (if (string-match hou-regexp file-path)
 		 (progn (find-file (concat
 				    (match-string 1 file-path)
