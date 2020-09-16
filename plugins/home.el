3ff2a1b0a29c07db7c85a558a2d1368952179dc1
Fredrik Salomonsson <plattfot@posteo.net>
2020-09-16 14:26:34 -0700

    Add function to refresh mail [home]

--- plugins/home.el
+++ plugins/home.el
@@ -15,5 +15,20 @@
     (make-frame)
     (make-frame)))
 
+(defun home-mail-refresh ()
+  "Run mbsync+notmuch to refresh mail."
+  (interactive)
+  (async-shell-command "mbsync -ac $XDG_HOME_CONFIG/isync/config; notmuch new")
+  (set-process-sentinel (get-buffer-process "*Async Shell Command*")
+                        (lambda (process event)
+                          (when (eq (process-status process) 'exit)
+                            (with-current-buffer "*Async Shell Command*"
+                              (save-excursion
+                                (goto-char (point-max))
+                                (next-line -3)
+                                (message "Mail synced: %s" (buffer-substring
+                                                            (point-at-bol)
+                                                            (point-at-eol)))))))))
+
 (provide 'home)
 ;;; home.el ends here
